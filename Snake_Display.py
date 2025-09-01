c34fcd92458a329f4186ef3522cf7e3fb27c441f
NathansSoftwareDevelopment <210070614+NathansSoftwareDevelopment@users.noreply.github.com>
2025-09-01 13:33:47 -0500

    Added a "displayApple" function
    I realize this function is almost identical to the "clearPixel" function. This is to allow the "displayApple" function to accommodate an image in the future.

--- Snake_Display.py
+++ Snake_Display.py
@@ -40,3 +40,8 @@ blackRGB: tuple = (0, 0, 0)
 def clearPixel(xCoordinate: int, yCoordinate: int):
     pygame.draw.rect(display, blackRGB, pygame.grid[xCoordinate][yCoordinate])
     pygame.draw.rect(display, whiteRGB, pygame.grid[xCoordinate][yCoordinate], 1)
+
+redRGB: tuple = (255, 0, 0)
+def displayApple(xCoordinate: int, yCoordinate: int):
+    pygame.draw.rect(display, redRGB, pygame.grid[xCoordinate][yCoordinate])
+    pygame.draw.rect(display, whiteRGB, pygame.grid[xCoordinate][yCoordinate], 1)
